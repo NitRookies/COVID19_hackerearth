a7174e28f613895db87d62437967f70acf256e9e
NitRookies <63367887+NitRookies@users.noreply.github.com>
2020-04-18 20:04:49 +0530

    Add files via upload

---
+++ asia.php
@@ -0,0 +1,5 @@
+<?php include_once('header.php');?>
+
+
+<iframe src="asia1.html" width="100%" height="670" frameborder="0" scrolling="yes"></iframe>
+<iframe src="asia2.html" width="100%" height="670" frameborder="0" scrolling="yes"></iframe>
