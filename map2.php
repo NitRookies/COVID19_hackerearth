24892c5441a70cbf9d8e530752cd8539e38d9dd4
Arijit Saha Ray <52858374+Arijitsaharay@users.noreply.github.com>
2020-05-22 20:42:54 +0530

    Update map2.php

--- map2.php
+++ map2.php
@@ -1,2 +1,2 @@
 <?php include_once('header.php');?>
-<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1ooeGZ1loqS6_78H4gWVVnKtG2Io7wLhL" width="100%" height="680"></iframe>
+<iframe src="https://www.google.com/maps/d/u/2/embed?mid=1JuaTNxVQe_iMR4roWZJM_3Wxw5XmdiBg" width="100%" height="680"></iframe>
